be9bae8c5ab62f2571c40a6c86b3fc1983c4f8ec
Shivam Bansal <shivam5992@gmail.com>
2017-04-20 23:14:03 +0530

    cnn

--- cnn.py
+++ cnn.py
@@ -16,7 +16,6 @@ dropout = 0.75
 
 ## ?? # Dropout, probability to keep units
 
-
 x = tf.placeholder(tf.float32, [None, n_input])
 y = tf.placeholder(tf.float32, [None, n_classes])
 keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
@@ -47,19 +46,20 @@ def conv_net(x, weights, biases, dropout):
 
     fc1 = tf.nn.dropout(fc1, dropout)
     out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-    return out
+    return out 
 
 
 
 # Store layers weight & bias
 weights = {
     # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
     # 5x5 conv, 32 inputs, 64 outputs
-    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
     # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
     # 1024 inputs, 10 outputs (class prediction)
+    
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
     'out': tf.Variable(tf.random_normal([1024, n_classes]))
 }
 
